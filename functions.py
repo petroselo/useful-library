731ef39c7954ae511ea529522d8c04503c4ee1a2
Hannan Saddiq <saddiqhb@gmail.com>
2018-04-25 17:17:07 +0100

    Added function to return array of digits of a number

---
+++ functions.py
@@ -0,0 +1,6 @@
+# a grouping of useful python functions for the TPP coding challenge
+
+
+def number_to_digit_array(number):
+    array_to_return = [int(digit) for digit in str(number)]
+    return array_to_return
